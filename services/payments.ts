607cff56c87d300846638811abd5058f82a88248
LalbaAnthony <anthony.lalba@limayrac.fr>
2025-10-07 16:28:33 +0200

    Suite

--- services/payments.ts
+++ services/payments.ts
@@ -3,4 +3,5 @@ import { Payment } from "@/types/Payment";
 export const payments: Record<string, Payment> = {
     card: { name: "Carte Bancaire", class: "bg-blue-100 text-blue-800" },
     paypal: { name: "PayPal", class: "bg-yellow-100 text-yellow-800" },
+    transfer: { name: "Virement Bancaire", class: "bg-purple-100 text-purple-800" },
 };
